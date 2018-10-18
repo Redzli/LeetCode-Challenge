21dc5c5165fb2ab7b08c70ca63cee1e2aff4f6b7
albat3ross <44248469+albat3ross@users.noreply.github.com>
2018-10-17 21:09:11 -0400

    Add files via upload

---
+++ 2_han.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+void f1(char *a[]){
+	printf("%s\n",a[0]);
+}
+
+void f2(int *a){
+	int *b=malloc(sizeof(int));
+	*b=2;
+	a=b;
+	printf("in f2, a=%d\n",*a);
+}
+
+void main(int argc, char *argv[]){
+	//char *greetings[4]={"hi","yo","hello","bonjour"};
+	//f1(greetings);
+	int a = 3;
+	f2(&a);
+	printf("after f2, a=%d\n",a);
+}
